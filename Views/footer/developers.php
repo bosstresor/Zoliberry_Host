85a6f3e203f440c4cf9565c721e7d26a55a3a3ff
bosstresor <158502455+bosstresor@users.noreply.github.com>
2024-03-06 15:33:18 +0200

    Update developers.php

--- Views/footer/developers.php
+++ Views/footer/developers.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Developers</title>
+    <title>Developers of Zoliberry</title>
 
     <link rel="stylesheet" href="/sass/navbar.css">
     <link rel="stylesheet" href="/sass/FooterElementsCorrections.css">
