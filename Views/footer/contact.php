848e23861a174809d939a7e835e0e4d9d4e0507e
bosstresor <158502455+bosstresor@users.noreply.github.com>
2024-03-26 10:19:27 +0200

    Update contact.php

--- Views/footer/contact.php
+++ Views/footer/contact.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
+    <title>Contact Zoliberry</title>
 
     <link rel="stylesheet" href="/sass/navbar.css">
     <link rel="stylesheet" href="/sass/Home.css">
